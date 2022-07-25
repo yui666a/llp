c250b8b3c67bf807c0d71b0171c904528149a78b
yui666a <haiso666@gmail.com>
2022-07-25 20:52:45 +0900

    add calendar page

---
+++ pages/calendar.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <v-calendar
+        ref="calendar"
+        :events="events"
+        :event-color="getEventColor"
+        @change="getEvents"
+      />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'CalendarPage',
+  data() {
+    return {
+      mentalConditionNumber: 0,
+      bodyConditionNumber: 0,
+    }
+  },
+  methods: {
+    onClickBodyCondition(event) {
+      this.bodyConditionNumber = event === this.bodyConditionNumber ? 0 : event
+    },
+    onClickMentalCondition(event) {
+      this.mentalConditionNumber =
+        event === this.mentalConditionNumber ? 0 : event
+    },
+    confirmCondition() {
+      // TODO: ここにDB登録処理を書く
+      console.log(`mentalConditionNumber: ${this.mentalConditionNumber}`)
+      console.log(`bodyConditionNumber: ${this.bodyConditionNumber}`)
+      this.$router.push('/')
+    },
+  },
+}
+</script>
