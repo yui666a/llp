744e4ad80b44257635c71d918f61e3f91aa4a6d8
yui666a <haiso666@gmail.com>
2022-07-25 21:13:38 +0900

    add base url at inspire.vue

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row>
     <v-col class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <img :src="`${base}v.png`" alt="Vuetify.js" class="mb-5" />
       <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
         <footer>
@@ -17,5 +17,15 @@
 <script>
 export default {
   name: 'InspirePage',
+  data() {
+    return {
+      base: '',
+    }
+  },
+  mounted() {
+    const environment = process.env.NODE_ENV || 'local'
+    const env = require(`../env/${environment}.ts`)
+    this.base = env.BASE_URL
+  },
 }
 </script>
