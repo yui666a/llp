320cc6ca423467096bc774b80b1ecf67a4075571
takara <takarasakai00@gmail.com>
2022-07-26 13:38:37 +0900

    練習abcd

---
+++ pages/abcd.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img :src="`${base}v.png`" alt="Vuetify.js" class="mb-5" />
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'InspirePage',
+  data() {
+    return {
+      base: '',
+    }
+  },
+  mounted() {
+    const environment = process.env.NODE_ENV || 'local'
+    const env = require(`../env/${environment}.ts`)
+    this.base = env.BASE_URL
+  },
+}
+</script>
