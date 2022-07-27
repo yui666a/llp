2aa3903d3d83716666f8b229dcb664f9b952ada7
yui666a <haiso666@gmail.com>
2022-07-27 16:41:41 +0900

    アプリ名の変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,8 +23,8 @@ const config: NuxtConfig = {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - llp-app',
-    title: 'llp-app',
+    titleTemplate: '%s - ハリンタ',
+    title: 'ハリンタ',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
