3ee9d7a222c863088f99d96589b67e9431224a5d
yui666a <haiso666@gmail.com>
2022-07-28 19:43:00 +0900

    グラフ用データを取得

--- pages/condition.vue
+++ pages/condition.vue
@@ -8,7 +8,7 @@
         :chart-options="options"
       />
       <div class="text-center">
-        <v-btn depressed small @click="fillData()">Randomize</v-btn>
+        <v-btn depressed small @click="getData">更新</v-btn>
       </div>
     </v-col>
   </v-row>
@@ -21,44 +21,21 @@ export default {
     return {
       chartdata: {
         datacollection: {
-          labels: [
-            '2021-08-02 09:30',
-            '2021-08-02 11:10',
-            '2021-08-02 13:00',
-            '2021-08-02 15:00',
-            '2021-08-02 18:30',
-            '2021-08-02 19:50',
-          ],
+          labels: [],
           datasets: [
             {
               label: 'ココロ',
-              data: [3, 3, 4, 3, 2, 4],
+              data: [],
               borderColor: 'rgba(255, 99, 132, 1)',
               backgroundColor: 'rgba(255, 99, 132, 0.1)',
             },
             {
               label: 'カラダ',
-              data: [4, 4, 3, 3, 2, 4],
+              data: [],
               borderColor: 'rgba(132, 99, 255, 1)',
               backgroundColor: 'rgba(132, 99, 255, 0.1)',
             },
           ],
-
-          // labels: ['data1', 'data2', 'data3', 'data4', 'data5', 'data6'],
-          // datasets: [
-          //   {
-          //     label: 'ココロ',
-          //     data: [4, 2, 4, 3, 2, 5],
-          //     borderColor: 'rgba(255, 99, 132, 1)',
-          //     backgroundColor: 'rgba(255, 99, 132, 0.1)',
-          //   },
-          //   {
-          //     label: 'カラダ',
-          //     fill: true,
-          //     backgroundColor: 'blue',
-          //     data: [3, 5, 1, 2, 3, 5],
-          //   },
-          // ],
         },
       },
       options: {
@@ -67,28 +44,31 @@ export default {
         legend: {
           display: false,
         },
-        // scales: {
-        //   xAxes: [
-        //     {
-        //       type: 'time',
-        //       time: {
-        //         parser: 'mm:ss',
-        //         unit: 'minute',
-        //         unitStepSize: 1,
-        //         displayFormats: {
-        //           minute: 'mm',
-        //         },
-        //       },
-        //     },
-        //   ],
-        //   yAxes: [
-        //     {
-        //       ticks: {
-        //         beginAtZero: true,
-        //       },
-        //     },
-        //   ],
-        // },
+        scales: {
+          //   xAxes: [
+          //     {
+          //       type: 'time',
+          //       time: {
+          //         parser: 'mm:ss',
+          //         unit: 'minute',
+          //         unitStepSize: 1,
+          //         displayFormats: {
+          //           minute: 'mm',
+          //         },
+          //       },
+          //     },
+          //   ],
+          yAxes: [
+            {
+              ticks: {
+                min: 0, // 最小値
+                beginAtZero: true,
+                max: 25, // 最大値
+                stepSize: 5, // 間隔
+              },
+            },
+          ],
+        },
       },
     }
   },
@@ -103,43 +83,29 @@ export default {
       const url = `/gas/macros/s/AKfycbxPUw73VyxjmzRw96UVsIpuPUlDvI9qFYF4MzpwOecly-KTiQrBv9jwu0sUVZ49b09N/exec?type=conditions&lineId=${lineId}`
       const response = await this.$axios.$get(url)
       console.log(response)
-    },
-    fillData() {
-      console.log('before fillData')
-      console.log(this.chartdata.datacollection.datasets[0].data)
-      this.chartdata.datacollection.datasets[0].data = [
-        Math.floor(Math.random() * 10),
-        Math.floor(Math.random() * 10),
-        Math.floor(Math.random() * 10),
-        Math.floor(Math.random() * 10),
-        Math.floor(Math.random() * 10),
-        Math.floor(Math.random() * 10),
-      ]
-      console.log('after fillData')
-      console.log(this.chartdata.datacollection.datasets[0].data)
-      // this.datacollection = {
-      //   labels: [this.getRandomInt(), this.getRandomInt()],
-      //   datasets: [
-      //     {
-      //       label: 'Test Data1',
-      //       backgroundColor: 'rgba(255, 100, 130, 0.2)',
-      //       data: [this.getRandomInt(), this.getRandomInt()],
-      //     },
-      //     {
-      //       label: 'Test Data2',
-      //       backgroundColor: 'rgba(100, 130, 255, 0.2)',
-      //       data: [this.getRandomInt(), this.getRandomInt()],
-      //     },
-      //     {
-      //       label: 'Test Data3',
-      //       backgroundColor: 'rgba(130, 255, 100, 0.2)',
-      //       data: [this.getRandomInt(), this.getRandomInt()],
-      //     },
-      //   ],
-      // }
-    },
-    getRandomInt() {
-      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+
+      // labelの設定
+      let labels = response.data.map(
+        (d) =>
+          d[0].substring(5, 10).replace('-', '/') + ' ' + d[1].substring(11, 16)
+      )
+      labels = Array.from(new Set(labels))
+
+      // dataの設定
+      const mentalData = response.data
+        .filter((d) => {
+          return d[4] === 'こころ'
+        })
+        .map((d) => d[5])
+      const bodyData = response.data
+        .filter((d) => {
+          return d[4] === 'からだ'
+        })
+        .map((d) => d[5])
+
+      this.chartdata.datacollection.labels = labels
+      this.chartdata.datacollection.datasets[0].data = mentalData
+      this.chartdata.datacollection.datasets[1].data = bodyData
     },
   },
 }
